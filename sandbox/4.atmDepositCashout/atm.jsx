6fe77f5b1513b579f8e19ff4467ce4f3642c8de2
gaiadg <78041477+gaiadg@users.noreply.github.com>
2021-08-06 00:48:52 +0200

    Add files via upload

---
+++ sandbox/4.atmDepositCashout/atm.jsx
@@ -0,0 +1,41 @@
+const Account = () => {
+    const [transaction, setTransaction] = React.useState(0);
+    const [currentBalance, setCurrentBalance] = React.useState(0);
+    const [isDeposit, setIsDeposit] = React.useState(true);
+    let status = `Account Balance $${currentBalance}`;
+    const handleChange = event => {
+        if (!isDeposit && Number(event.target.value > currentBalance)) {
+            return alert ("You don't have enough funds");
+        };
+        setTransaction(Number(event.target.value));
+        console.log(isDeposit);
+    };
+    const handleSubmit = () => {
+        console.log(isDeposit);
+        let newBalance = isDeposit ? currentBalance + transaction : currentBalance - transaction;
+        setCurrentBalance(newBalance);
+        event.preventDefault();
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 id="total">{status}</h2>
+            <button onClick={()=> setIsDeposit(true)}>Deposit</button>
+            <button onClick={()=> setIsDeposit(false)}>Cash Back</button>
+            <ATMDeposit onChange={handleChange} isDeposit={isDeposit}> Deposit</ATMDeposit>
+        </form>
+    );
+};
+
+const ATMDeposit = ({ onChange, isDeposit }) => {
+    const transactionType = ['Deposit', "Cash Back"];
+    console.log(transactionType[Number(!isDeposit)]);
+    return (
+        <label className="label huge">
+            <h3>{transactionType[Number(!isDeposit)]}</h3>
+            <input type="number" onChange={onChange}></input>
+            <input type="submit" value="Submit"></input>
+        </label>
+    );
+};
+
+ReactDOM.render(<Account />, document.getElementById('root'));
